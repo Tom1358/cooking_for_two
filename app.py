eb1c816bdb53515a9afeb2f3ead5cf40c14c7ec3
Tom1358 <thomasellis358@hotmail.com>
2021-09-19 09:52:50 +0000

    Create directory templates, and within that, dishes.html

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, 
+    request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid  import ObjectId
 if os.path.exists("env.py"):
@@ -16,9 +18,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/recipes")
-def hello():
-    return "Hello World!"
+@app.route("/dishes")
+def dishes():
+    dishes = mongo.db.dishes.find()
+    return render_template("dishes.html", dishes=dishes)
 
 
 if __name__ == "__main__":
