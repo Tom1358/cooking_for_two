9c8463a1c95ee4124cf2eaa8a38795c666b13c09
Tom1358 <thomasellis358@hotmail.com>
2021-10-17 08:55:05 +0000

    Add edit adn delete functionality to individual_recipe.html by adding hrefs in HTML

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,21 @@
 let addIngredientDiv = document.getElementById("add-ingredient-div");
 
+// document.getElementById("minus-ingredient").addEventListener('click', minusIngredientFunction);
+
+
+
+
 function addIngredient(){
     console.log("Add button clicked")
     let newIngredient = `
-        <div class="input-group form-group">
+        <div class="input-group form-group extra-item">
 
         <div class="input-group-prepend">
             <span class="input-group-text"><i class="fas fa-utensil-spoon"></i></span>
         </div>
         
         <input id="ingredients" name="ingredients" type="text" class="form-control validate" minlength="5"
-            maxlength="35" placeholder="Ingredient ${1}" required>
+            maxlength="35" placeholder="" required>
         
         <div class="input-group-prepend">
             <span class="input-group-text"><a id="minus-ingredient"><i class="fas fa-minus"></i></a></span>
@@ -21,11 +26,12 @@ function addIngredient(){
     addIngredientDiv.innerHTML += newIngredient
 }
 
-function minusIngredient(){
+function minusIngredientFunction(){
     console.log("Minus button clicked")
-    newIngredient.remove(this);
+    // let deleteItem = document.getElementById("extra-item")
+    // this.remove(deleteItem);
 }
 
-document.getElementById("add-ingredient").addEventListener("click", addIngredient);
+// document.getElementById("add-ingredient").addEventListener("click", addIngredient);
 
-document.getElementById("minus-ingredient").addEventListener("click", minusIngredient);
+
